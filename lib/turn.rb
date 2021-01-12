2a51545c9e5668b8232198cc94b78de64e70a53f
Kristyn Little <kristynlittle@frontier.com>
2021-01-12 03:32:18 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,10 +32,11 @@ end
 
 def turn_count(board)
   counter = 0
-  board.each do |spaces|
-    if spaces == "X" || spaces == "O"
+  board.each do {|space|
+    if space == "X" || space == "O"
       counter += 1
     end
+  }
     counter
   end
 
