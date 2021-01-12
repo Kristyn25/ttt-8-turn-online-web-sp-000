012c3dd1c6d49989f04c4335bd8f001e8b3b85d2
Kristyn Little <kristynlittle@frontier.com>
2021-01-12 03:20:12 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,6 +34,15 @@ def player(board)
   turn_count(board) % 2 == 0? "X" : "O"
 end
 
+def turn_count(board)
+  counter = 0
+  board.each do |spaces|
+    if spaces == "X" || spaces == "O"
+      counter += 1
+    end
+    counter
+  end
+
 def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.strip
